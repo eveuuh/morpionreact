1d4b6a76499f0531f9bd406f880a05c04906853e
eveuuh <eve.jourdan@gmail.com>
2020-09-29 16:35:16 +0200

    git init

---
+++ src/components/index.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+
+
+export default function Morpion() {
+    return (
+        <table>
+            <tr>
+                <td>O</td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td>o</td>
+                <td>o</td>
+                <td></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+        </table>
+    )
+}
